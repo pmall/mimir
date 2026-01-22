2d3e8966ea85cc1bb5381b0a5265cf3564f8617b
pmall <pierre.mallinjoud@gmail.com>
2026-01-22 13:00:14 +0100

    fix: update batch size default to 8 for T4 compatibility

--- scripts/train.py
+++ scripts/train.py
@@ -342,7 +342,7 @@ def train(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--epochs", type=int, default=1)
-    parser.add_argument("--batch_size", type=int, default=64)
+    parser.add_argument("--batch_size", type=int, default=8)
     parser.add_argument("--lr", type=float, default=1e-4)
     parser.add_argument("--lora_r", type=int, default=8)
     parser.add_argument("--lora_alpha", type=int, default=32)
